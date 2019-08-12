ec3ea797b7cf64978b34f91cf85d5377c51c10d7
László Holler <fuzzyhead8@gmail.com>
2019-08-12 15:16:51 +0200

    GD-02
    Animations

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,29 +1,32 @@
 extends KinematicBody2D
 
 var motion = Vector2()
+const UP = Vector2(0,-1)
+const SPEED = 200
+const GRAVITY = 30
+const JUMP_HEIGHT = -650 
 
 func _physics_process(delta):
-	
+	motion.y += GRAVITY
 	
 	# Move the character Left or Right		
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
+		$Sprite.flip_h = false
+		#get_node("Sprite")flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100	
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
 		motion.x = 0
-	
+		$Sprite.play("Idle")
 		
-	# Move the character Up or Down		
-	if Input.is_action_pressed("ui_up"):
-		motion.y = -100	
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = 100		
-	else:
-		motion.y = 0
-		
-	motion.y += 20
-
-	move_and_slide(motion)	
-	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	else:		
+		$Sprite.play("Jump")
+	move_and_slide(motion, UP)	
 	pass
